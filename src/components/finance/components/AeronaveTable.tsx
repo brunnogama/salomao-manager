c99e745910e34c9a43db303cfeaa8f0e7de5ea84
Bruno Gama <30611127+brunnogama@users.noreply.github.com>
2026-02-02 17:47:24 -0300

    ss

--- src/components/finance/components/AeronaveTable.tsx
+++ src/components/finance/components/AeronaveTable.tsx
@@ -51,7 +51,8 @@ export function AeronaveTable({ data, loading, onRowClick }: AeronaveTableProps)
             <th className="px-4 py-4 text-[10px] font-black uppercase tracking-widest">Fornecedor</th>
             <th className="px-4 py-4 text-[10px] font-black uppercase tracking-widest">Destino</th>
             <th className="px-4 py-4 text-[10px] font-black uppercase tracking-widest">Despesa</th>
-            <th className="px-4 py-4 text-[10px] font-black uppercase tracking-widest">Descrição</th>            
+            <th className="px-4 py-4 text-[10px] font-black uppercase tracking-widest">Descrição</th>
+            
             <th className="px-4 py-4 text-[10px] font-black uppercase tracking-widest">Faturado CNPJ</th>
             <th className="px-4 py-4 text-[10px] font-black uppercase tracking-widest">Previsto</th>
             <th className="px-4 py-4 text-[10px] font-black uppercase tracking-widest">Pago</th>
