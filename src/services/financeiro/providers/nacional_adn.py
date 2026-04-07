9c461aad892e45e5c9510a1c54e985f3534356c0
brunnogama <brunnogama@gmail.com>
2026-04-06 21:48:18 -0300

    fix(nfse): bind cTribMun strictly to 3 digits compliant with TCCodTribMun XML schema rng6110 constraint

--- src/services/financeiro/providers/nacional_adn.py
+++ src/services/financeiro/providers/nacional_adn.py
@@ -97,11 +97,12 @@ class NacionalAdnProvider:
         
         # Dados do Servico
         # Revertemos para 171401 (que o Nacional entende) mas como o RJ exige administração municipal vinculada,
-        # vamos injetar a tag <cTribMun> localmente também!
         c_trib_raw = servico.get('codigo_tributacao', '171401').replace('.', '').replace('-', '')
         c_trib_nac = c_trib_raw[:6].zfill(6)
-        # Conforme evidência do Portal Nacional (RJ): Código Complementar de Advocacia é '17.14.01.001'
-        c_trib_mun = ((c_trib_raw + '001') if c_trib_raw == '171401' else c_trib_raw).zfill(20)
+        
+        # O Padrão Nacional exige que cTribMun seja estritamente 3 dígitos (xs:pattern value="[0-9]{3}").
+        # No RJ, o código complementar para 17.14.01 é '001'.
+        c_trib_mun = '001' if c_trib_raw == '171401' else '000'
             
         c_nbs = servico.get('codigo_nbs', '113019000').replace('.', '').replace('-', '')
         desc = servico.get('discriminacao', 'Honorários Advocatícios')
